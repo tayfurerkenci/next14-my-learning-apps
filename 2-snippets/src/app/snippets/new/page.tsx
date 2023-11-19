cff1d62e1e8101028d2a70bb57e4298273d6756e
Tayfur <terkenci@gmail.com>
2023-11-19 19:28:00 +0300

    feat: created snippets project

---
+++ 2-snippets/src/app/snippets/new/page.tsx
@@ -0,0 +1,35 @@
+export default function SnippetCreatePage() {
+  return (
+    <form>
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            Title
+          </label>
+          <input
+            className="border rounded p-2 w-full"
+            type="text"
+            id="title"
+            name="title"
+          />
+        </div>
+
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
+            Code
+          </label>
+          <textarea
+            className="border rounded p-2 w-full"
+            id="code"
+            name="code"
+          />
+        </div>
+
+        <button className="border rounded p-2 bg-blue-200" type="submit">
+          Create
+        </button>
+      </div>
+    </form>
+  );
+}
