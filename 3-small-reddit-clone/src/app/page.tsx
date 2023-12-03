66215c2adc2d02db81fa96309c5a3e42a9632c60
Tayfur <terkenci@gmail.com>
2023-12-03 20:30:30 +0300

    feat: created TopicList component

--- 3-small-reddit-clone/src/app/page.tsx
+++ 3-small-reddit-clone/src/app/page.tsx
@@ -1,4 +1,7 @@
+import { Divider } from '@nextui-org/react';
+
 import TopicCreateForm from '@/components/topics/topic-create-form';
+import TopicList from '@/components/topics/topic-list';
 
 export default function Home() {
   return (
@@ -6,8 +9,11 @@ export default function Home() {
       <div className="col-span-3">
         <h1 className="text-xl m-2">Top Posts</h1>
       </div>
-      <div>
+      <div className="border shadow py-3 px-2">
         <TopicCreateForm />
+        <Divider className="my-2" />
+        <h3 className="text-lg">Topics</h3>
+        <TopicList />
       </div>
     </div>
   );
